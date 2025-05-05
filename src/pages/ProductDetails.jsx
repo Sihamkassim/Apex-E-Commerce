49144a73a51a1cf5ab055f170ba0c1369f5ef3a2
siham <sihamkassim735@gmail.com>
2025-05-05 11:23:41 +0300

    added a static /dummy data/

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,11 @@
 
 const ProductDetails = () => {
   return (
-    <div>
+    <div className="product-details container mx-auto p-4 flex flex-col items-center ">
+      
+      <h1 className="text-2xl font-bold mb-4">Product Details</h1>
+
+    
       pd
     </div>
   )
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,35 @@
-
+import merchandise from "../constants/StaticProducts";
 const Products = () => {
   return (
-    <div>
-      p
+    <div className="grid grid-cols sm:grid-cols-2 lg:grid-cols-5 gap-3 w-full">
+      {merchandise.map((merchandise, i) => (
+        <div
+          key={i}
+          className="bg-[#EDEDED] hover:scale-105 rounded-xl border border-gray-200 shadow-md p-4 hover:shadow-lg transition-all relative"
+        >
+          {merchandise.suggestion && (
+            <span className="absolute top-2 right-2 bg-yellow-400 text-white text-xs font-semibold px-2 py-1 rounded">
+              Recommended
+            </span>
+          )}
+          <img
+            src={merchandise.src}
+            alt={merchandise.name}
+            className="w-full h-48 object-contain mb-3 rounded-md bg-gray-50"
+          />
+          <h2 className="text-lg font-semibold line-clamp-2">
+            {merchandise.name}
+          </h2>
+          <img src={merchandise.review} alt="rating" className="h-4 mb-1" />
+          <p className="text-gray-500 text-xs mb-2">{merchandise.stock}</p>
+          <p className="text-black font-bold mt-2">{merchandise.price}</p>
+          <button className="w-full mt-4 bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition">
+            View Details
+          </button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
