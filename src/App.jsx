95b05e46f4f02298c3f9de1d93a3c6607208adcb
Natnael Esayas <Nanoboss44@gmail.com>
2025-05-06 18:49:10 +0200

    add api integration and solve merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -8,84 +8,10 @@ import Login from "./pages/Login.jsx";
 import ProductDetails from "./pages/ProductDetails.jsx";
 import Products from "./pages/Products.jsx";
 import Register from "./pages/Register.jsx";
-<<<<<<< HEAD
-import AdminPanel from "./pages/Admin.jsx";
-import AdminLayout from "./components/Common/AdminLayout";
-import { AppSidebar } from "./components/Sidebar components/app-sidebar";
-import AdminProduct from "./pages/AdminProduct";
-=======
-
->>>>>>> 1245245d7ff8bcc88e115fb3132985cdbffacb35
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <Home />
-            </Layout>
-          }
-        />
-        <Route
-          path="/products"
-          element={
-            <Layout>
-              <Products />
-            </Layout>
-          }
-        />
-        <Route
-          path="/products/:id"
-          element={
-            <Layout>
-              <ProductDetails />
-            </Layout>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <Layout>
-              <Login />
-            </Layout>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <Layout>
-              <Register />
-            </Layout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <Layout>
-              <About />
-            </Layout>
-          }
-        />
-        <Route
-          path="/AdminPanel"
-          element={
-            <AdminLayout>
-              <AdminPanel />
-            </AdminLayout>
-          }
-        />
-        <Route
-          path="/AdminProduct"
-          element={
-            <AdminLayout>
-              <AdminProduct />
-            </AdminLayout>
-          }
-        />
-=======
         <Route path="/" element={<Layout><Home /></Layout>} />
         <Route path="/products" element={<Layout><Products /></Layout>} />
         <Route path="/products/:name" element={<Layout><ProductDetails /></Layout>} />
@@ -93,7 +19,6 @@ function App() {
         <Route path="/register" element={<Layout><Register /></Layout>} />
         <Route path="/about" element={<Layout><About /></Layout>} />
         <Route path="/AdminPanel" element={<AdminLayout><AdminPanel /></AdminLayout>} />
->>>>>>> 1245245d7ff8bcc88e115fb3132985cdbffacb35
       </Routes>
     </BrowserRouter>
   );
